d38a3213dcb4e8530215a9c9e0dc83c7b8953913
Sean Jezewski <seanwjezewski@gmail.com>
2013-06-13 22:21:02 -0700

    Try a root route

--- cloud/app.js
+++ cloud/app.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req,res) { res.redirect('/submission/new') });
+
 // Global app configuration section
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine

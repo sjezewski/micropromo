89be318b546781dc03e141f4155cac89ed14116a
Sean Jezewski <seanwjezewski@gmail.com>
2013-06-13 23:25:26 -0700

    Add promo check controller

--- cloud/controllers/promo.js
+++ cloud/controllers/promo.js
@@ -19,4 +19,34 @@ exports.create = function(req, res) {
 exports.check = function(req, res) {
   // Run query to see if promo code exists. 
   // Simple yes/no response is sufficient
+
+  var query = new Parse.Query(Promo);
+  var promo = new Promo();
+  promo.id = req.query.promo;
+  query.equalTo("objectId", promo.id);  
+
+  var success = {success: true};
+  var promoError = {error: true, message: "Nonexistent promo code"};
+
+  query.first(
+    {
+      success: function(thispromo) {
+
+        if(thispromo === undefined) {
+          res.send(500, JSON.stringify(promoError));          
+          return
+        }
+
+        // TODO: In the future you'd also check here that the promo hasn't been used
+
+        res.send(200, JSON.stringify(success));
+
+      },
+      error: function(error) {
+        promoError.details = error;
+        res.send(500, JSON.stringify(error));     
+      }
+    }
+  );
+
 }
